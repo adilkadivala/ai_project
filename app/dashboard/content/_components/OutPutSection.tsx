4d22fb5142859885c9220260147bc21eef830d35
k-adi <adilkadivala560@gmail.com>
2024-07-10 20:41:42 +0530

    ai api installetion

--- app/dashboard/content/_components/OutPutSection.tsx
+++ app/dashboard/content/_components/OutPutSection.tsx
@@ -1,16 +1,31 @@
-require("@toast-ui/editor/dist/toastui-editor.css");
+import { Button } from "@/components/ui/button";
+import { Copy } from "lucide-react";
+import { useRef } from "react";
 
+require("@toast-ui/editor/dist/toastui-editor.css");
 const { Editor } = require("@toast-ui/react-editor");
 
 const OutPutSection = () => {
+  const editorRef: any = useRef();
+
   return (
-    <div className="bg-white">
+    <div className="bg-white shadow-lg border rounded-lg ">
+      <div className="flex justify-between items-center p-5">
+        <h2 className="font-medium text-lg">Your Result</h2>
+
+        <Button className="flex gap-2">
+          <Copy className="w-4 h-4" /> Copy
+        </Button>
+      </div>
       <Editor
-        initialValue="hello react editor world!"
-        previewStyle="vertical"
+        ref={editorRef}
+        initialValue="Your Result will appear here"
         height="600px"
-        initialEditType="markdown"
+        initialEditType="wysiwyg"
         useCommandShortcut={true}
+        onChange={() =>
+          console.log(editorRef.current.getInstance().getMarkdown())
+        }
       />
     </div>
   );
