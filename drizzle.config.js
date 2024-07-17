8bf3a1e168805237bd91221231bc84d33063c446
k-adi <adilkadivala560@gmail.com>
2024-07-17 17:24:32 +0530

    dtabase implements

---
+++ drizzle.config.js
@@ -0,0 +1,7 @@
+export default {
+  schema: "./utils/schema.tsx",
+  dialect: "postgresql",
+  dbCredentials: {
+    url: "postgresql://neondb_owner:7sXFWwY5jEGi@ep-fancy-leaf-a5q8lwoq.us-east-2.aws.neon.tech/neondb?sslmode=require",
+  },
+};
