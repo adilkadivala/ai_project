d861dc7f4d0edcc24d810d5e124a2ceaaef57846
k-adi <adilkadivala560@gmail.com>
2024-07-21 15:32:32 +0530

    getting databse data in history file

--- app/dashboard/_components/SideNav.tsx
+++ app/dashboard/_components/SideNav.tsx
@@ -1,7 +1,9 @@
 "use client";
 import { FileClock, Home, Settings, WalletCards } from "lucide-react";
 import Image from "next/image";
+import Link from "next/link";
 import { usePathname } from "next/navigation";
+import UserTrack from "./UserTrack";
 
 const SideNav = () => {
   const MenuList = [
@@ -30,7 +32,7 @@ const SideNav = () => {
   const path = usePathname();
 
   return (
-    <div className="h-screen p-5 shadow-sm border bg-white">
+    <div className="relative h-screen p-5 shadow-sm border bg-white">
       <div className="flex justify-center gap-1 ">
         <Image src={"/logo.svg"} alt="brand" width={30} height={30} />
         <p className="font-semibold text-lg text-sky-600">Dashboard</p>
@@ -40,16 +42,22 @@ const SideNav = () => {
 
       <div className="mt-3">
         {MenuList.map((menu, index) => (
-          <div
-            className={`flex gap-2 mb-2 p-3 hover:bg-primary hover:text-white rounded-lg cursor-pointer items-center ${
-              path == menu.path && "bg-primary text-white"
-            }`}
-            key={index}
-          >
-            <menu.icon className="h-6 w-6" />
-            <h2 className="text-lg">{menu.name}</h2>
-          </div>
+          <Link href={menu.path}>
+            <div
+              className={`flex gap-2 mb-2 p-3 hover:bg-primary hover:text-white rounded-lg cursor-pointer items-center ${
+                path == menu.path && "bg-primary text-white"
+              }`}
+              key={index}
+            >
+              <menu.icon className="h-6 w-6" />
+              <h2 className="text-lg">{menu.name}</h2>
+            </div>
+          </Link>
         ))}
+      </div>
+
+      <div className="absolute bottom-10 w-full left-0">
+        <UserTrack />
       </div>
     </div>
   );
